1f14ac9f8701b31b27deb48b13eacb03409d1b0a
Mohamed Dawoud <mhmaddawoud20@gmail.com>
2024-02-07 19:52:22 +0200

    Adding the delete function

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -55,7 +55,7 @@ class FileStorage:
     def delete(self, obj=None):
         """Deletes obj from __objects if it's inside"""
         if obj is not None:
-            key = obj.__class__.__name__ + '.' + obj.id
+            key = f"{obj.__class__.__name__}.{obj.id}"
             if key in FileStorage.__objects:
                 del FileStorage.__objects[key]
-                self.save()
+            self.save()
