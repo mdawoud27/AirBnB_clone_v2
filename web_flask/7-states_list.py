0b99ef87d6a78d73cdbe0f286c39bf76681d234c
Mohamed Dawoud <mhmaddawoud20@gmail.com>
2024-02-16 21:09:35 +0200

    Adding some files

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""script that starts a Flask web application"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.route("/states_list")
+def display_states():
+    """Function that displays a html page with the states listed"""
+    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
+    return render_template("7-states_list.html", states=states)
+
+
+@app.teardown_appcontext
+def close_storage():
+    """Close the storage"""
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
