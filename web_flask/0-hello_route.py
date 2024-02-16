a801fd9bda0eaa1056390d9e82020e9628ec6cb6
Mohamed Dawoud <mhmaddawoud20@gmail.com>
2024-02-16 14:41:21 +0200

    Adding 0-hello_route.py, __init__.py files

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""script that starts a Flask web application"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def say_hello():
+    """Function that says hello"""
+    return "Hello HBNB!"
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
