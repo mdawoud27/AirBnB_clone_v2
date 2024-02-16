cd0d275fd9aad08ea4a5b68995210cb005e7e812
Mohamed Dawoud <mhmaddawoud20@gmail.com>
2024-02-16 14:53:19 +0200

    Adding c text file

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""script that starts a Flask web application"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def say_hello():
+    """Function that says hello"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def say_hbnb():
+    """Function that says hbnb"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def say_c_is(text=None):
+    """Function that say some text about C."""
+    text.replace(' ', '_')
+    return f'C {text}'
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
