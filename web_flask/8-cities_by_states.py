176af746c4cacb84e12b3b9e194a1e256394a46d
Mohamed Dawoud <mhmaddawoud20@gmail.com>
2024-02-17 09:23:48 +0200

    Adding Cities by states script

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""script that starts a Flask web application"""
+from flask import Flask, render_template
+from models import storage
+
+app = Flask(__name__)
+
+
+@app.route("/cities_by_states", strict_slashes=False)
+def display_states():
+    """Function that displays a html page with the states listed"""
+    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
+    return render_template("8-cities_by_states.html", states=states)
+
+
+@app.teardown_appcontext
+def close_storage():
+    """Close the storage"""
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
